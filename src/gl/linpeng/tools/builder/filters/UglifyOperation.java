6b9cdbd2c4a8eb4fde854d71f068903a4ff8cf7d
magixsource <magixsource.cn@gmail.com>
2016-04-22 15:47:51 +0800

    [M] add uglify operation(not finish)

---
+++ src/gl/linpeng/tools/builder/filters/UglifyOperation.java
@@ -0,0 +1,44 @@
+package gl.linpeng.tools.builder.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gl.linpeng.tools.builder.resources.BasicResource;
+import gl.linpeng.tools.builder.resources.BuilderResource;
+import gl.linpeng.tools.builder.service.ResourceType;
+
+/**
+ * Uglify resource
+ * 
+ * @author linpeng
+ *
+ */
+public class UglifyOperation implements BuilderOperation {
+	final Logger logger = LoggerFactory.getLogger(UglifyOperation.class);
+	private String content = "";
+
+	@Override
+	public void onProcess(BuilderResource resource) {
+		BasicResource br = (BasicResource) resource;
+		logger.info("Processing resource [{}]", br.getSource().getName());
+		String original = br.getContent();
+		
+		// begin process
+		
+		
+		br.setContent(original);
+		content += original;
+	}
+
+	@Override
+	public boolean isSupported(BuilderResource resource) {
+		return resource.getType().equals(ResourceType.JavaScript)
+				|| resource.getType().equals(ResourceType.Css);
+	}
+
+	@Override
+	public String toText() {
+		return this.content;
+	}
+
+}
