3c5aca5677d0fdea1d04e399481eac6cc0e63233
magixsource <magixsource.cn@gmail.com>
2016-04-27 16:01:19 +0800

    [A]add two buildresult implements

---
+++ src/gl/linpeng/tools/builder/result/FrontendBuildResult.java
@@ -0,0 +1,21 @@
+package gl.linpeng.tools.builder.result;
+
+/**
+ * Frontend build result parse result as a zip outputstream
+ * 
+ * @author linpeng
+ *
+ */
+public class FrontendBuildResult implements BuildResult {
+
+	@Override
+	public BuildResult toResult() {
+		return null;
+	}
+
+	@Override
+	public String toText() {
+		return null;
+	}
+
+}
---
+++ src/gl/linpeng/tools/builder/result/MavenBuildResult.java
@@ -0,0 +1,21 @@
+package gl.linpeng.tools.builder.result;
+
+/**
+ * Maven build result parse result as a pom.xml
+ * 
+ * @author linpeng
+ *
+ */
+public class MavenBuildResult implements BuildResult {
+
+	@Override
+	public BuildResult toResult() {
+		return null;
+	}
+
+	@Override
+	public String toText() {
+		return null;
+	}
+
+}
