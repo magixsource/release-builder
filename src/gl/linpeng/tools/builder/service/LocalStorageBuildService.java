df7d84334fe637c66a0f11fd77d78de6501de380
magixsource <magixsource.cn@gmail.com>
2016-04-27 15:57:12 +0800

    [A]add localstorage buildservice

---
+++ src/gl/linpeng/tools/builder/service/LocalStorageBuildService.java
@@ -0,0 +1,21 @@
+package gl.linpeng.tools.builder.service;
+
+import gl.linpeng.tools.builder.module.LocalStorageModule;
+
+import java.util.List;
+
+/**
+ * A build service for local storage scene
+ * 
+ * @author linpeng
+ *
+ */
+public interface LocalStorageBuildService extends BuildService {
+
+	/**
+	 * load localstorage modules
+	 * 
+	 * @return LocalStorageModule collections
+	 */
+	public List<LocalStorageModule> loadLocalStorageModules();
+}
